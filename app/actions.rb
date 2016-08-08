3d903dd9649c7a5308633be7a44452044a077598
Chanwoo Kim <chanwoohill@gmail.com>
2016-08-08 18:48:19 +0000

    add form and http request

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,22 @@
 get '/' do
   erb :index
 end
+
+get '/songs' do 
+  @songs = Song.all 
+  erb :'songs/index' 
+end 
+
+get '/songs/new' do
+  erb :'songs/new'
+end 
+
+post '/songs' do 
+  @song = Song.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  @song.save 
+  redirect '/songs'
+end 
